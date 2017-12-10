5eee699e152d1268cfcc5176dbbb6f452c15e437
DESKTOP-B8HMU4B\germaine <cpwongak@connect.ust.hk>
2017-12-10 21:48:57 +0800

    no message

--- sorting_top.v
+++ sorting_top.v
@@ -50,43 +50,181 @@ module sorting_top(
     wire [15:0] cells_push;
     
     
+   sorting_cell cell1(
+         .clk(clk),.rst(rst),.en(en),
+         .prev_data(31'b0),
+         .new_data(input_data),
+         .prev_push(1'b0), //always unpushed
+         .prev_state(1'b1), //always occupied
+         .cell_data(cells_data[0]),
+         .cell_push(cells_push[0]),
+         .cell_state(cells_state[0])
+    );
+    sorting_cell cell2(
+          .clk(clk),.rst(rst),.en(en),
+          .prev_data(cells_data[0]),
+          .new_data(input_data),
+          .prev_push(cells_push[0]),
+          .prev_state(cells_state[0]),
+          .cell_data(cells_data[1]),
+          .cell_push(cells_push[1]),
+          .cell_state(cells_state[1])
+    );
     
-    generate 
-    genvar  i;
-        for(i=0; i<SIZE; i=i+1) 
-        begin : sorting
-            if( i == 0)  //first sorting cell
-            begin
-                sorting_cell(
-                .clk(clk),.rst(rst),.en(en),
-                .prev_data('b0),
-                .new_data(input_data),
-                .prev_push('b0), //always unpushed
-                .prev_state('b1), //always occupied
-                .cell_data(cells_data[i]),
-                .cell_push(cells_push[i]),
-                .cell_state(cells_state[i])
-                );
-            end
+    sorting_cell cell3(
+          .clk(clk),.rst(rst),.en(en),
+          .prev_data(cells_data[1]),
+          .new_data(input_data),
+          .prev_push(cells_push[1]),
+          .prev_state(cells_state[1]),
+          .cell_data(cells_data[2]),
+          .cell_push(cells_push[2]),
+          .cell_state(cells_state[2])
+    );
+        
+    sorting_cell cell4(
+          .clk(clk),.rst(rst),.en(en),
+          .prev_data(cells_data[2]),
+          .new_data(input_data),
+          .prev_push(cells_push[2]),
+          .prev_state(cells_state[2]),
+          .cell_data(cells_data[3]),
+          .cell_push(cells_push[3]),
+          .cell_state(cells_state[3])
+    ); 
+    
+    sorting_cell cell5(
+          .clk(clk),.rst(rst),.en(en),
+          .prev_data(cells_data[3]),
+          .new_data(input_data),
+          .prev_push(cells_push[3]),
+          .prev_state(cells_state[3]),
+          .cell_data(cells_data[4]),
+          .cell_push(cells_push[4]),
+          .cell_state(cells_state[4])
+     ); 
+        
+    sorting_cell cell6(
+           .clk(clk),.rst(rst),.en(en),
+           .prev_data(cells_data[4]),
+           .new_data(input_data),
+           .prev_push(cells_push[4]),
+           .prev_state(cells_state[4]),
+           .cell_data(cells_data[5]),
+           .cell_push(cells_push[5]),
+           .cell_state(cells_state[5])
+     ); 
             
-            else
-            begin
-                sorting_cell(
-                .clk(clk),.rst(rst),.en(en),
-                .prev_data(cells_data[i-1]),
-                .new_data(input_data),
-                .prev_push(cells_push[i-1]),
-                .prev_state(cells_state[i-1]),
-                .cell_data(cells_data[i]),
-                .cell_push(cells_push[i]),
-                .cell_state(cells_state[i])
-                );
-            end
-        end
-    endgenerate
+            sorting_cell cell7(
+                              .clk(clk),.rst(rst),.en(en),
+                              .prev_data(cells_data[5]),
+                              .new_data(input_data),
+                              .prev_push(cells_push[5]),
+                              .prev_state(cells_state[5]),
+                              .cell_data(cells_data[6]),
+                              .cell_push(cells_push[6]),
+                              .cell_state(cells_state[6])
+                ); 
+                
+                sorting_cell cell8(
+                                  .clk(clk),.rst(rst),.en(en),
+                                  .prev_data(cells_data[6]),
+                                  .new_data(input_data),
+                                  .prev_push(cells_push[6]),
+                                  .prev_state(cells_state[6]),
+                                  .cell_data(cells_data[7]),
+                                  .cell_push(cells_push[7]),
+                                  .cell_state(cells_state[7])
+                    ); 
+                    sorting_cell cell9(
+                                      .clk(clk),.rst(rst),.en(en),
+                                      .prev_data(cells_data[7]),
+                                      .new_data(input_data),
+                                      .prev_push(cells_push[7]),
+                                      .prev_state(cells_state[7]),
+                                      .cell_data(cells_data[8]),
+                                      .cell_push(cells_push[8]),
+                                      .cell_state(cells_state[8])
+                        ); 
+                        sorting_cell cell10(
+                                          .clk(clk),.rst(rst),.en(en),
+                                          .prev_data(cells_data[8]),
+                                          .new_data(input_data),
+                                          .prev_push(cells_push[8]),
+                                          .prev_state(cells_state[8]),
+                                          .cell_data(cells_data[9]),
+                                          .cell_push(cells_push[9]),
+                                          .cell_state(cells_state[9])
+                            ); 
+                            sorting_cell cell11(
+                                              .clk(clk),.rst(rst),.en(en),
+                                              .prev_data(cells_data[9]),
+                                              .new_data(input_data),
+                                              .prev_push(cells_push[9]),
+                                              .prev_state(cells_state[9]),
+                                              .cell_data(cells_data[10]),
+                                              .cell_push(cells_push[10]),
+                                              .cell_state(cells_state[10])
+                                ); 
+                                sorting_cell cell12(
+                                                  .clk(clk),.rst(rst),.en(en),
+                                                  .prev_data(cells_data[10]),
+                                                  .new_data(input_data),
+                                                  .prev_push(cells_push[10]),
+                                                  .prev_state(cells_state[10]),
+                                                  .cell_data(cells_data[11]),
+                                                  .cell_push(cells_push[11]),
+                                                  .cell_state(cells_state[11])
+                                    ); 
+                                    sorting_cell cell13(
+                                                      .clk(clk),.rst(rst),.en(en),
+                                                      .prev_data(cells_data[11]),
+                                                      .new_data(input_data),
+                                                      .prev_push(cells_push[11]),
+                                                      .prev_state(cells_state[11]),
+                                                      .cell_data(cells_data[12]),
+                                                      .cell_push(cells_push[12]),
+                                                      .cell_state(cells_state[12])
+                                        ); 
+                                        sorting_cell cell14(
+                                                          .clk(clk),.rst(rst),.en(en),
+                                                          .prev_data(cells_data[12]),
+                                                          .new_data(input_data),
+                                                          .prev_push(cells_push[12]),
+                                                          .prev_state(cells_state[12]),
+                                                          .cell_data(cells_data[13]),
+                                                          .cell_push(cells_push[13]),
+                                                          .cell_state(cells_state[13])
+                                            );  
+                                            sorting_cell cell15(
+                                                              .clk(clk),.rst(rst),.en(en),
+                                                              .prev_data(cells_data[13]),
+                                                              .new_data(input_data),
+                                                              .prev_push(cells_push[13]),
+                                                              .prev_state(cells_state[13]),
+                                                              .cell_data(cells_data[14]),
+                                                              .cell_push(cells_push[14]),
+                                                              .cell_state(cells_state[14])
+                                                ); 
+                                                sorting_cell cell16(
+                                                                  .clk(clk),.rst(rst),.en(en),
+                                                                  .prev_data(cells_data[14]),
+                                                                  .new_data(input_data),
+                                                                  .prev_push(cells_push[14]),
+                                                                  .prev_state(cells_state[14]),
+                                                                  .cell_data(cells_data[15]),
+                                                                  .cell_push(cells_push[15]),
+                                                                  .cell_state(cells_state[15])
+                                                    ); 
+         
+ 
+
+
+
+
     
     //output the sorting result
-    always @(posedge clk)
+    always @(cells_data )
     begin
         sorted_data1 <= cells_data[0];
         sorted_data2 <= cells_data[1];
